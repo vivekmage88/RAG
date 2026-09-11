76bd05689c47153a9497fab1048673f72449652d
Vivek Sharma <vivekvksh88@gmail.com>
2026-09-12 02:10:03 +0530

    Wire embedding and answer caches into the query path

--- retrieve.py
+++ retrieve.py
@@ -1,4 +1,5 @@
 from store import collection, embed_texts
+from cache import get_cached_embedding, set_cached_embedding
 
 MAX_DISTANCE = 1.2
 
@@ -30,6 +31,20 @@ def search_relevant(question:str, n_results: int = 5, max_distance: float = MAX_
                 "distance": round(distance[i], 3),
             })
     return matches
+
+# calling Cache function
+
+from cache import get_cached_embedding, set_cached_embedding
+
+
+def get_query_vector(question: str) -> list[float]:
+    cached = get_cached_embedding(question)
+    if cached is not None:
+        return cached
+
+    vector = embed_texts([question])[0]
+    set_cached_embedding(question, vector)
+    return vector
     
 
 # Testing Phase
